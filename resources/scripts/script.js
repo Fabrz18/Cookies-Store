f2b587886273b88d4dc638b34d6b082f2bf9c95d
Fabrz18 <fabriziogarrogarcia@gmail.com>
2025-01-07 10:16:12 -0500

    Responsive solved

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,4 +1,36 @@
+   
+
 window.addEventListener("resize", function() {
+    const variable1 =  this.document.getElementById("options")
+    const variable2 = this.document.getElementById("icon")
+    const variable3 = this.document.getElementById("emergency-options")
+    const logo = this.document.getElementById("logo")
+    const logo_ico = this.document.getElementById("logo-ico")
+    if (window.innerWidth <= 500) {
+        console.log("El ancho de la ventana es menor o igual a 450px.");
+        variable1.style.display  = "none";
+        variable2.style.display  = "none";
+        variable3.style.display  = "flex";
+        logo.style.width = "38px"
+        logo.style.height = "38px"
+        
+        logo_ico.style.width = "38px"
+        logo_ico.style.height = "38px"
+
+        } else {
+        console.log("El ancho de la ventana es mayor a 600px.");
+        variable1.style.display = "flex"
+        variable2.style.display = "flex"
+        variable3.style.display = "none"
+        logo.style.width =  "69px"
+        logo.style.height = "69px"
+   
+        logo_ico.style.width =  "69px"
+        logo_ico.style.height = "69px"
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
     const variable1 =  this.document.getElementById("options")
     const variable2 = this.document.getElementById("icon")
     const variable3 = this.document.getElementById("emergency-options")
