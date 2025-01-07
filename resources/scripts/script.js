4952cb90639504244577eebe36bd5b042b60b5d4
Fabrz18 <fabriziogarrogarcia@gmail.com>
2025-01-07 10:04:55 -0500

    New navbar responsive and logo added

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -2,16 +2,28 @@ window.addEventListener("resize", function() {
     const variable1 =  this.document.getElementById("options")
     const variable2 = this.document.getElementById("icon")
     const variable3 = this.document.getElementById("emergency-options")
-    if (window.innerWidth <= 450) { // Cambia 600 por el ancho deseado
+    const logo = this.document.getElementById("logo")
+    const logo_ico = this.document.getElementById("logo-ico")
+    if (window.innerWidth <= 500) {
         console.log("El ancho de la ventana es menor o igual a 450px.");
         variable1.style.display  = "none";
         variable2.style.display  = "none";
         variable3.style.display  = "flex";
+        logo.style.width = "38px"
+        logo.style.height = "38px"
         
-    } else {
+        logo_ico.style.width = "38px"
+        logo_ico.style.height = "38px"
+
+        } else {
         console.log("El ancho de la ventana es mayor a 600px.");
         variable1.style.display = "flex"
         variable2.style.display = "flex"
         variable3.style.display = "none"
+        logo.style.width =  "69px"
+        logo.style.height = "69px"
+   
+        logo_ico.style.width =  "69px"
+        logo_ico.style.height = "69px"
     }
 });
